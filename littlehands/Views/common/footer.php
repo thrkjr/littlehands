52c14cffc8808646e0b3ab9be5057c21b628fc1d
thrkjr <89626335+thrkjr@users.noreply.github.com>
2021-08-27 18:17:39 +0900

    Delete littlehands directory

--- littlehands/Views/common/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-<p>利用規約　|　プライバシーポリシー　|　お問い合わせ　|　運営会社　|　サイトマップ</p>
-<p>COPYRIGHT &copy; 2021 littlehands Inc. ALL RIGHTS RESERVED.</p>
-</footer>
--- littlehands/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST','localhost'); 		// データベースのホスト名又はIPアドレス
-define('DB','littlehands');         // データベース名
-define('USER','root');			    // MySQLのユーザ名
-define('PASSWORD','root'); 	// MySQLのパスワード
-define('CHARSET','utf8');           // 文字コード
-?>
